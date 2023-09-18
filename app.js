2f7789e897f8ed99560b5e83f0c404b9877af004
Takuiya-dev <example@gmail.com>
2023-09-19 02:13:02 +0900

    Added universal style footer

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ let html = '';
 for (let i = 1; i <= projectsNumber; i++) {
     html += `<a href="./project-${i}/index.html" aria-current="Project ${i}">Project ${i}</a>`
 }
-nav.innerHTML = html
+nav.innerHTML = html;
---
+++ project-2/app.js
@@ -0,0 +1,29 @@
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.center-text {
+    text-align: center;
+}
+
+
+header {
+    position: fixed;
+    top: 15%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: right;
+}
+
+header nav {
+    display: flex;
+    align-items: center;
+    justify-content: right;
+}
+
+menu {
+    list-style: none;
+}

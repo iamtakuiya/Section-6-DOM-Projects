d7731d83dc697116dd494c01fe021b9a8e335b54
Takuiya-dev <example@gmail.com>
2023-09-19 04:09:44 +0900

    Completed section-2: Counter

--- project-2/app.js
+++ project-2/app.js
@@ -1,29 +1,47 @@
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-.center-text {
-    text-align: center;
-}
+// set initial count
+let count = 0;
 
 
-header {
-    position: fixed;
-    top: 15%;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: right;
+// Select value buttons
+const btns = document.querySelectorAll('.btn');
+let value = document.querySelector('#value');
+
+function checkBtn(styles) {
+    if (styles.contains('btn--increase')) {
+        count++;
+    } else if (styles.contains('btn--decrease')) {
+        count--;
+    } else if (styles.contains('btn--reset')) {
+        count = 0;
+    }
+    value.textContent = count;
+    changeColor(count);
 }
 
-header nav {
-    display: flex;
-    align-items: center;
-    justify-content: right;
+// count is global variable, so it's not necessary to pass the argument.
+// I do this for practical purpose
+function changeColor(count) {
+    switch (true) {
+        case (count > 0):
+            value.style.color = 'hsl(131, 69%, 56%)';
+            break;
+        case (count < 0):
+            value.style.color = 'hsl(0, 86%, 57%)';
+            break;
+        case (count === 0):
+            value.style.color = '#222';
+            break;
+        default:
+    }
+    console.log(count)
 }
 
-menu {
-    list-style: none;
-}
+btns.forEach(btn => { 
+    btn.addEventListener('click', (e) => {
+        // console.log(e.currentTarget.textContent + ': clicked')
+        // Get the current target classList
+        const styles = e.currentTarget.classList;
+        checkBtn(styles);
+      
+    })
+})

57e345a670c579ab78868f61aea13e4a8f0d916b
Takuiya-dev <example@gmail.com>
2023-12-15 01:36:34 +0900

    map through products

--- app/20-filters/tutorial/app.js
+++ app/20-filters/tutorial/app.js
@@ -1,1 +1,26 @@
-console.log('filters project');
+// import products from '../tutorial/products.js';
+
+let filteredProducts = [...products];
+
+const productsContainer = document.querySelector('.products-container');
+
+const displayProducts = () => {
+	// destructuring access each key properties
+	productsContainer.innerHTML = filteredProducts
+		.map(({ id, title, image, price }) => {
+			/**
+			 * Key place into each attribute by using string template.
+			 */
+			return `
+            <article class="product" data-id="${id}">
+            <img src="${image}" alt="" class="product-img img" />
+            <footer>
+              <h5 class="product-name">${title}</h5>
+              <span class="product-price">${price}</span>
+            </footer>
+          </article>
+    `;
+		})
+		.join('');
+};
+displayProducts();

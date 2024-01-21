ea764cb86873a6d8f15701fe38ece7f28581921a
Takuiya-dev <example@gmail.com>
2024-01-21 13:33:14 +0900

    Added getUser and get() fucntiosn

--- app/23-random-user/tutorial/app.js
+++ app/23-random-user/tutorial/app.js
@@ -1,1 +1,19 @@
-const URL = 'https://randomuser.me/api/';
+import get from './utils/getElement.js';
+import fetchUser from './utils/fetchuser.js';
+
+const img = get('.user-img');
+const title = get('.user-title');
+const value = get('.user-value');
+const btn = get('.btn');
+const btns = [...document.querySelectorAll('.icon')];
+
+const showUser = async () => {
+	// Invoke the getUser async func
+	// getUser().then((data) => console.log(data));
+
+	const person = await getUser();
+	console.log(person);
+};
+
+window.addEventListener('DOMContentLoaded', showUser);
+btn.addEventListener('click', showUser);
